b37441c72e62c3c3a27f8dd0c9e0890a414bd596
Raunak Bhimsaria <raunakbhimsaria@Raunaks-MacBook-Air-2.local>
2021-04-11 12:28:35 +0530

    done

--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -19,49 +19,16 @@ public class MainActivity extends AppCompatActivity {
     EditText minutesEditText;
     TextView timerText;
     TextView textTimeLeft;
+    int hoursDif, minDif;
 
     public void pressed(View view) {
         textTimeLeft.setVisibility(View.VISIBLE);
-        int hours = Integer.valueOf(hoursEditText.getText().toString());
-
-        int minutes = Integer.valueOf(minutesEditText.getText().toString());
-
-        //int minDif, hoursDif;
-        Date currentTime = Calendar.getInstance().getTime();
-        String t = new SimpleDateFormat("HH:mm:ss").format(currentTime);
-        String time[] = t.split(":");
-        System.out.println("value: " + time[0] + " " + time[1]);
-
-        int current = (Integer.valueOf(time[1]) + 60*Integer.valueOf(time[0]));
-        int alarmTime = (hours*60+minutes);
-        int timeDifference = alarmTime - current;
-        if (timeDifference < 0) {
-            timeDifference = 24 * 60 + timeDifference;
-        }
-        int hoursDif = timeDifference / 60;
-        int minDif = timeDifference % 60;
-
+        updateTime();
         int timeLeft = (hoursDif * 3600000) + (minDif * 60000);
-
-        System.out.println("value: " + timeLeft);
-
         new CountDownTimer(timeLeft, 60000) {
-
             @Override
             public void onTick(long millisUntilFinished) {
-                Date currentTime = Calendar.getInstance().getTime();
-                String t = new SimpleDateFormat("HH:mm:ss").format(currentTime);
-                String time[] = t.split(":");
-                int current = (Integer.valueOf(time[1]) + 60*Integer.valueOf(time[0]));
-                int alarmTime = (hours*60+minutes);
-                int timeDifference = alarmTime - current;
-                if (timeDifference < 0) {
-                    timeDifference = 24 * 60 + timeDifference;
-                }
-                int hoursDif = timeDifference / 60;
-                int minDif = timeDifference % 60;
-
-                int timeLeft = (hoursDif * 3600000) + (minDif * 60000);
+                updateTime();
                 if (minDif <= 9)
                     timerText.setText(Integer.toString(hoursDif) + " hours  : 0" + Integer.toString(minDif) + " minutes");
                 else
@@ -70,14 +37,29 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-
+                textTimeLeft.setVisibility(View.INVISIBLE);
+                timerText.setText("Time is up!");
                 MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.siren);
                 mediaPlayer.start();
             }
         }.start();
     }
 
-    //public void update
+    public void updateTime() {
+        int hours = Integer.valueOf(hoursEditText.getText().toString());
+        int minutes = Integer.valueOf(minutesEditText.getText().toString());
+        Date currentTime = Calendar.getInstance().getTime();
+        String t = new SimpleDateFormat("HH:mm:ss").format(currentTime);
+        String time[] = t.split(":");
+        int current = (Integer.valueOf(time[1]) + 60 * Integer.valueOf(time[0]));
+        int alarmTime = (hours * 60 + minutes);
+        int timeDifference = alarmTime - current;
+        if (timeDifference < 0)
+            timeDifference = 24 * 60 + timeDifference;
+        hoursDif = timeDifference / 60;
+        minDif = timeDifference % 60;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
